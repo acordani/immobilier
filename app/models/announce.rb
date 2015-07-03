f985eedb180da4b26d9f49602edf51935de90423
Alexandre Cordani <alcordani@gmail.com>
2015-07-03 12:13:45 +0200

    ajout pictures ds model announce

--- app/models/announce.rb
+++ app/models/announce.rb
@@ -56,10 +56,40 @@ class Announce < ActiveRecord::Base
   validates :title, presence: true
   validates :property_id, presence: true
   validates :description, presence: true
+ # Géocode
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
+ # Pictures
+
   has_attached_file :picture1,
     styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture1, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :picture1, less_than: 5.megabytes
+
+  has_attached_file :picture2,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture2, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :picture2, less_than: 5.megabytes
+
+
+  has_attached_file :picture3,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+
+
+  validates_attachment_content_type :picture3, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :picture3, less_than: 5.megabytes
+
+
+ has_attached_file :picture4,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+
+
+  validates_attachment_content_type :picture4, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :picture4, less_than: 5.megabytes
 end
