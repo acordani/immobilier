02c1e12ab2ec3a03fdac492843d6cdfdc36e727e
Alexandre Cordani <alcordani@gmail.com>
2015-07-04 12:07:39 +0200

    ss

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
-class AnnouncesController < ApplicationController
-
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
 
     @announces_select = Announce.all.select { |f| f.picture1.exists? }.take(3)
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :announces
   # resources :wicked, only: [:create, :show, :update]
-  resources :searches
+  # resources :searches
 end
