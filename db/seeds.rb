791f045392324bcdc08df419239de8923b673c25
ac <alcordani@gmail.com>
2017-05-07 09:20:09 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,10 @@ puts "Deleting old records..."
 Property.destroy_all
 Announce.destroy_all
 
-# puts "Creating property..."
-# Property.create(name: "Appartement")
-# Property.create(name: "Maison")
-# Property.create(name: "Local Commercial")
+puts "Creating property..."
+Property.create(name: "Appartement")
+Property.create(name: "Maison")
+Property.create(name: "Local Commercial")
 
 
 # puts "Creating Announces"
